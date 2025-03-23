b923c7c4b70223247afc04acab4d8933b0b79dff
hifichevymane <blekbjek@gmail.com>
2025-03-23 15:59:06 +0200

    7 завдання

--- js/show-ordered-soft-list.js
+++ js/show-ordered-soft-list.js
@@ -8,3 +8,9 @@ const elementList = document.querySelectorAll('.prof-soft-item');
 elementList.forEach((element, index) => {
   element.textContent = `${index + 1}. ${dataSite.proSoft[index]}`;
 });
+
+dataSite.whyUs.sort();
+const whyUsListElements = document.querySelectorAll('.list1 li a');
+whyUsListElements.forEach((element, index) => {
+  element.textContent = dataSite.whyUs[index];
+});
