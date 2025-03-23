d7b5fe70c101ca1f6b73cca80966ec6c74894a6a
hifichevymane <blekbjek@gmail.com>
2025-03-23 15:59:28 +0200

    format a bit

--- js/show-ordered-soft-list.js
+++ js/show-ordered-soft-list.js
@@ -2,8 +2,8 @@ const dataSite = {
   proSoft: ['Firewall', 'Antivirus', 'VPN'],
   whyUs: ['High quality', 'Reliable', 'Fast'],
 };
-dataSite.proSoft.sort();
 
+dataSite.proSoft.sort();
 const elementList = document.querySelectorAll('.prof-soft-item');
 elementList.forEach((element, index) => {
   element.textContent = `${index + 1}. ${dataSite.proSoft[index]}`;
